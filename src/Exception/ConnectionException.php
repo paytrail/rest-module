4e0c0ca30bd7ba0a90cb525a11b1366bec2dfa5b
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-23 12:20:37 +0300

    feat: add proper rest flow

--- src/Exception/ConnectionException.php
+++ src/Exception/ConnectionException.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Paytrail\Exception;
 
-/**
- * @author Paytrail <tech@paytrail.com>
- */
 class ConnectionException extends \Exception
 {
 }
--- src/Exception/PaytrailException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Paytrail\Exception;
-
-class PaytrailException extends \Exception
-{
-}
