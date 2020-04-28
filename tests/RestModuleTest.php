376102c0a69a0d7d5af5f1b3ffdadf811ad4a617
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-28 11:24:53 +0300

    test: add tests for RestModule and RestPayment

--- tests/RestModuleTest.php
+++ tests/RestModuleTest.php
@@ -17,211 +17,211 @@ use Prophecy\Prophet;
 
 class RestModuleTest extends TestCase
 {
-    const TOKEN = 'secretToken';
-    const PAYMENT_LINK = 'linkToPayment';
+        const TOKEN = 'secretToken';
+        const PAYMENT_LINK = 'linkToPayment';
 
-    const ORDER_NUMBER = 'Test-Payment-1234';
+        const ORDER_NUMBER = 'Test-Payment-1234';
 
-    private $product;
-    private $customer;
+        private $product;
+        private $customer;
 
-    private $prophet;
+        private $prophet;
 
-    public function setUp(): void
-    {
-        parent::setUp();
+        public function setUp(): void
+        {
+                parent::setUp();
 
-        $this->prophet = new Prophet();
+                $this->prophet = new Prophet();
 
 
 
-        $this->product = Product::create([
-            'title' => 'Foo',
-            'code' => '001',
-            'price' => 2,
-        ]);
-        $this->customer = Customer::create([
-            'firstName' => 'Foo',
-            'lastName' => 'Bar',
-            'email' => 'customer.email@nomail.com',
-            'street' => 'Foo',
-            'postalCode' => '1234',
-            'postalOffice' => 'Bar',
-            'country' => 'FI',
-        ]);
-    }
+                $this->product = Product::create([
+                        'title' => 'Foo',
+                        'code' => '001',
+                        'price' => 2,
+                ]);
+                $this->customer = Customer::create([
+                        'firstName' => 'Foo',
+                        'lastName' => 'Bar',
+                        'email' => 'customer.email@nomail.com',
+                        'street' => 'Foo',
+                        'postalCode' => '1234',
+                        'postalOffice' => 'Bar',
+                        'country' => 'FI',
+                ]);
+        }
 
-    private function getRestModule(RestPayment $payment = null)
-    {
-        $merchant = Merchant::create('13466', '6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ');
+        private function getRestModule(RestPayment $payment = null)
+        {
+                $merchant = Merchant::create('13466', '6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ');
 
-        $restClientMock = $this->prophet->prophesize();
-        $restClientMock->willExtend(RestClient::class);
-        $restClientMock->getResponse($payment)->willReturn($this->getRestResponse());
+                $restClientMock = $this->prophet->prophesize();
+                $restClientMock->willExtend(RestClient::class);
+                $restClientMock->getResponse($payment)->willReturn($this->getRestResponse());
 
-        return new RestModule($merchant, $restClientMock->reveal());
-    }
+                return new RestModule($merchant, $restClientMock->reveal());
+        }
 
-    private function getRestResponse()
-    {
-        $response = new \stdClass();
-        $response->url = self::PAYMENT_LINK;
-        $response->token = self::TOKEN;
+        private function getRestResponse()
+        {
+                $response = new \stdClass();
+                $response->url = self::PAYMENT_LINK;
+                $response->token = self::TOKEN;
 
-        return $response;
-    }
+                return $response;
+        }
 
-    public function tearDown(): void
-    {
-        parent::tearDown();
-        $this->prophet->checkPredictions();
-    }
+        public function tearDown(): void
+        {
+                parent::tearDown();
+                $this->prophet->checkPredictions();
+        }
 
-    public function testExceptionIsThrownWithoutOrderNumberOnPaymentLink()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ValidationException::class);
-        $restModule->getPaymentLink();
-    }
+        public function testExceptionIsThrownWithoutOrderNumberOnPaymentLink()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ValidationException::class);
+                $restModule->getPaymentLink();
+        }
 
-    public function testExceptionIsThrownWithoutOrderNumberOnPaymentWidget()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ValidationException::class);
-        $restModule->getPaymentWidget();
-    }
+        public function testExceptionIsThrownWithoutOrderNumberOnPaymentWidget()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ValidationException::class);
+                $restModule->getPaymentWidget();
+        }
 
-    public function testExceptionIsThrownWithoutProductsOrPrice()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ProductException::class);
-        $restModule->createPayment(self::ORDER_NUMBER);
-    }
+        public function testExceptionIsThrownWithoutProductsOrPrice()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ProductException::class);
+                $restModule->createPayment(self::ORDER_NUMBER);
+        }
 
-    public function testExceptionIsThrownWhenAddingProductWhenAmountIsSet()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ProductException::class);
-        $restModule->addPrice(10);
-        $restModule->addProducts([$this->product]);
-    }
+        public function testExceptionIsThrownWhenAddingProductWhenAmountIsSet()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ProductException::class);
+                $restModule->addPrice(10);
+                $restModule->addProducts([$this->product]);
+        }
 
-    public function testExceptionIsThrownWhenAddingAmountAndHasProducts()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ProductException::class);
-        $restModule->addProducts([$this->product]);
-        $restModule->addPrice(10);
-    }
+        public function testExceptionIsThrownWhenAddingAmountAndHasProducts()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ProductException::class);
+                $restModule->addProducts([$this->product]);
+                $restModule->addPrice(10);
+        }
 
-    public function testCustomerInformationWithPriceThrowsException()
-    {
-        $restModule = $this->getRestModule();
-        $this->expectException(ValidationException::class);
-        $restModule->addCustomer($this->customer);
-        $restModule->addPrice(10);
-    }
+        public function testCustomerInformationWithPriceThrowsException()
+        {
+                $restModule = $this->getRestModule();
+                $this->expectException(ValidationException::class);
+                $restModule->addCustomer($this->customer);
+                $restModule->addPrice(10);
+        }
 
-    public function testPaymentLinkIsCreated()
-    {
-        $payment = new RestPayment(self::ORDER_NUMBER, [], null, [], 10);
+        public function testPaymentLinkIsCreated()
+        {
+                $payment = new RestPayment(self::ORDER_NUMBER, [], null, [], 10);
 
-        $restModule = $this->getRestModule($payment);
+                $restModule = $this->getRestModule($payment);
 
-        $restModule->addPrice(10);
-        $restModule->createPayment(self::ORDER_NUMBER, [], RestModule::TYPE_XML);
+                $restModule->addPrice(10);
+                $restModule->createPayment(self::ORDER_NUMBER, [], RestModule::TYPE_XML);
 
-        $paymentLink = $restModule->getPaymentLink();
+                $paymentLink = $restModule->getPaymentLink();
 
-        $this->assertSame(self::PAYMENT_LINK, $paymentLink);
-    }
+                $this->assertSame(self::PAYMENT_LINK, $paymentLink);
+        }
 
-    public function testPaymentWidgetIsCreated()
-    {
-        $payment = new RestPayment(self::ORDER_NUMBER, [], $this->customer, [$this->product]);
+        public function testPaymentWidgetIsCreated()
+        {
+                $payment = new RestPayment(self::ORDER_NUMBER, [], $this->customer, [$this->product]);
 
-        $restModule = $this->getRestModule($payment);
+                $restModule = $this->getRestModule($payment);
 
-        $restModule->addCustomer($this->customer);
-        $restModule->addProducts([$this->product]);
-        $restModule->createPayment(self::ORDER_NUMBER);
+                $restModule->addCustomer($this->customer);
+                $restModule->addProducts([$this->product]);
+                $restModule->createPayment(self::ORDER_NUMBER);
 
-        $paymentLink = $restModule->getPaymentWidget();
+                $paymentLink = $restModule->getPaymentWidget();
 
-        $this->assertStringContainsString(self::TOKEN, $paymentLink);
-        $this->assertStringContainsString(RestModule::WIDGET_URL, $paymentLink);
-    }
+                $this->assertStringContainsString(self::TOKEN, $paymentLink);
+                $this->assertStringContainsString(RestModule::WIDGET_URL, $paymentLink);
+        }
 
-    public function testIsPaidReturnsCorrectValues()
-    {
-        $notPaidReturnParameters = [
-            'ORDER_NUMBER' => self::ORDER_NUMBER,
-            'TIMESTAMP' => '1588058158',
-            'RETURN_AUTHCODE' => 'B1370EB96F52DD1EDB2B3400807A6597',
-        ];
+        public function testIsPaidReturnsCorrectValues()
+        {
+                $notPaidReturnParameters = [
+                        'ORDER_NUMBER' => self::ORDER_NUMBER,
+                        'TIMESTAMP' => '1588058158',
+                        'RETURN_AUTHCODE' => 'B1370EB96F52DD1EDB2B3400807A6597',
+                ];
 
-        $paidReturnParameters = [
-            'ORDER_NUMBER' => self::ORDER_NUMBER,
-            'TIMESTAMP' => '1588058042',
-            'PAID' => 'da9974de9f',
-            'METHOD' => 1,
-            'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
-        ];
+                $paidReturnParameters = [
+                        'ORDER_NUMBER' => self::ORDER_NUMBER,
+                        'TIMESTAMP' => '1588058042',
+                        'PAID' => 'da9974de9f',
+                        'METHOD' => 1,
+                        'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
+                ];
 
-        $restModule = $this->getRestModule();
+                $restModule = $this->getRestModule();
 
-        $this->assertFalse($restModule->isPaid($notPaidReturnParameters));
-        $this->assertTrue($restModule->isPaid($paidReturnParameters));
-    }
+                $this->assertFalse($restModule->isPaid($notPaidReturnParameters));
+                $this->assertTrue($restModule->isPaid($paidReturnParameters));
+        }
 
-    public function testReturnAuhtcodeIsCorrect()
-    {
-        $notPaidReturnParameters = [
-            'ORDER_NUMBER' => self::ORDER_NUMBER,
-            'TIMESTAMP' => '1588058158',
-            'RETURN_AUTHCODE' => 'B1370EB96F52DD1EDB2B3400807A6597',
-        ];
+        public function testReturnAuhtcodeIsCorrect()
+        {
+                $notPaidReturnParameters = [
+                        'ORDER_NUMBER' => self::ORDER_NUMBER,
+                        'TIMESTAMP' => '1588058158',
+                        'RETURN_AUTHCODE' => 'B1370EB96F52DD1EDB2B3400807A6597',
+                ];
 
-        $paidReturnParameters = [
-            'ORDER_NUMBER' => self::ORDER_NUMBER,
-            'TIMESTAMP' => '1588058042',
-            'PAID' => 'da9974de9f',
-            'METHOD' => 1,
-            'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
-        ];
+                $paidReturnParameters = [
+                        'ORDER_NUMBER' => self::ORDER_NUMBER,
+                        'TIMESTAMP' => '1588058042',
+                        'PAID' => 'da9974de9f',
+                        'METHOD' => 1,
+                        'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
+                ];
 
-        $restModule = $this->getRestModule();
+                $restModule = $this->getRestModule();
 
-        $this->assertTrue($restModule->returnAuthcodeIsValid($notPaidReturnParameters));
-        $this->assertTrue($restModule->returnAuthcodeIsValid($paidReturnParameters));
-    }
+                $this->assertTrue($restModule->returnAuthcodeIsValid($notPaidReturnParameters));
+                $this->assertTrue($restModule->returnAuthcodeIsValid($paidReturnParameters));
+        }
 
-    public function testReturnAuhtcodeIsInvalidWhenParameterIsMissing()
-    {
-        $returnParameters = [
-            'TIMESTAMP' => '1588058042',
-            'PAID' => 'da9974de9f',
-            'METHOD' => 1,
-            'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
-        ];
+        public function testReturnAuhtcodeIsInvalidWhenParameterIsMissing()
+        {
+                $returnParameters = [
+                        'TIMESTAMP' => '1588058042',
+                        'PAID' => 'da9974de9f',
+                        'METHOD' => 1,
+                        'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B134C3',
+                ];
 
-        $restModule = $this->getRestModule();
+                $restModule = $this->getRestModule();
 
-        $this->assertFalse($restModule->returnAuthcodeIsValid($returnParameters));
-    }
+                $this->assertFalse($restModule->returnAuthcodeIsValid($returnParameters));
+        }
 
-    public function testReturnAuhtcodeIsInvalidWhenCalculatedSumDoesNotMatch()
-    {
-        $returnParameters = [
-            'ORDER_NUMBER' => self::ORDER_NUMBER,
-            'TIMESTAMP' => '1588058042',
-            'PAID' => 'da9974de9f',
-            'METHOD' => 1,
-            'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B1AAAA',
-        ];
+        public function testReturnAuhtcodeIsInvalidWhenCalculatedSumDoesNotMatch()
+        {
+                $returnParameters = [
+                        'ORDER_NUMBER' => self::ORDER_NUMBER,
+                        'TIMESTAMP' => '1588058042',
+                        'PAID' => 'da9974de9f',
+                        'METHOD' => 1,
+                        'RETURN_AUTHCODE' => '8D9F70E16ACC86876E0A2FF806B1AAAA',
+                ];
 
-        $restModule = $this->getRestModule();
+                $restModule = $this->getRestModule();
 
-        $this->assertFalse($restModule->returnAuthcodeIsValid($returnParameters));
-    }
+                $this->assertFalse($restModule->returnAuthcodeIsValid($returnParameters));
+        }
 }
