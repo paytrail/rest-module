5abb7dc5c20c3a6592af3d638cf6b5e82f360c95
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-28 11:32:10 +0300

    fix: remove unused exception, fix dockblocks

--- src/Exception/ConnectionException.php
+++ src/Exception/ConnectionException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Paytrail\Exception;
 
+/**
+ * @package rest-module
+ * @author Paytrail <tech@paytrail.com>
+ */
 class ConnectionException extends \Exception
 {
 }
